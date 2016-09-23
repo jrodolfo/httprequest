48df8890834fd42ee9231de931286809442009b6
Rod Oliveira <jrodolfo@gmail.com>
2016-09-23 08:57:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# HTTP request GET/POST in Java
+
+From: How to send HTTP request GET/POST in Java
+https://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
+
---
+++ src/main/java/com/jrodolfo/httprequest/App.java
@@ -0,0 +1,13 @@
+package com.jrodolfo.httprequest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
